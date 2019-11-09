5dc5503df8f34b3de74ef3925136b0e2fe451b88
hikerrobot <hikerrobot@gmail.com>
2019-11-09 11:35:06 +0000

    Added script changes, which were missed from the last commit.

--- scripts/ring_pushover.sh
+++ scripts/ring_pushover.sh
@@ -3,22 +3,20 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-lloytron="7778920"
-cheapas_doorbell="-2086369837"
 cd $SCRIPTPATH
 while true;  do
 	scan=`./RPi_utils/RFSniffer`
 
-	if [ "$scan" = "$lloytron" ]; then
+	if [ "$scan" = "$DOORBELL_CODE" ]; then
 		timenow=`date`
-		echo $timenow "Good Read - lloytron"
+		echo $timenow " - Good Read :-)"
 		curl -s \
-		  --form-string "token=aastho4zszvsn1aki7968w7kaj8zdq" \
-		  --form-string "user=u7sw6hd7cabawudzqf5347ssq4d2x4" \
-		  --form-string "message=somebody's at the door!" \
+		  --form-string "token=$PUSHOVER_TOKEN" \
+		  --form-string "user=$PUSHOVER_UID" \
+		  --form-string "message=$PUSHOVER_MSG" \
 		  https://api.pushover.net/1/messages.json
 	else
-		echo "BAD READ:  discovered code = " $scan
+		echo $timenow " - BAD READ:  discovered code = " $scan
 	fi
 	sleep 5
 done
